013228f3528fa94389e2b3653b0231095d41d51f
Makabruh <milly@roach.me.uk>
2024-02-08 16:45:01 +0000

    Admin display

--- django_getting_started/meeting_planner/meetings/models.py
+++ django_getting_started/meeting_planner/meetings/models.py
@@ -2,7 +2,13 @@ from datetime import time
 from django.db import models
 
 class Meeting(models.Model):
+    #The database fields
     title = models.CharField(max_length=200)
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
+
+    #Needed double underscores here
+    #Displays friendly titles in the admin interface
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
