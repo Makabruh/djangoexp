955898c0b9cdd222f49af76d67993f8bd42b69f4
Makabruh <milly@roach.me.uk>
2024-02-08 17:00:01 +0000

    Removing data to restructure database

--- django_getting_started/meeting_planner/meetings/models.py
+++ django_getting_started/meeting_planner/meetings/models.py
@@ -1,18 +1,5 @@
 from datetime import time
 from django.db import models
-
-class Meeting(models.Model):
-    #The database fields
-    title = models.CharField(max_length=200)
-    date = models.DateField()
-    start_time = models.TimeField(default=time(9))
-    duration = models.IntegerField(default=1)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-
-    #Needed double underscores here
-    #Displays friendly titles in the admin interface
-    def __str__(self):
-        return f"{self.title} at {self.start_time} on {self.date}"
     
 class Room(models.Model):
     name = models.CharField(max_length=100)
@@ -21,3 +8,18 @@ class Room(models.Model):
 
     def __str__(self):
         return f"{self.name} is Room {self.room_number} on Floor {self.floor_number}"
+    
+class Meeting(models.Model):
+    #The database fields
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    #The CASCADE means all meetings set in a deleted room will also be deleted
+    #Cannot make a foreign key in Django without specifying on_delete behaviour
+
+    #Needed double underscores here
+    #Displays friendly titles in the admin interface
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
