0143c930ad114c4574b7d49c6da9d349fd12b54e
Makabruh <milly@roach.me.uk>
2024-02-08 16:24:11 +0000

    first commit

---
+++ django_getting_started/meeting_planner/meetings/models.py
@@ -0,0 +1,8 @@
+from datetime import time
+from django.db import models
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
