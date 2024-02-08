fd6dbed6f717a4665b40421accaa1746cc603458
Makabruh <milly@roach.me.uk>
2024-02-08 16:52:11 +0000

    Adding new model - Room

--- django_getting_started/meeting_planner/meetings/models.py
+++ django_getting_started/meeting_planner/meetings/models.py
@@ -7,8 +7,17 @@ class Meeting(models.Model):
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     #Needed double underscores here
     #Displays friendly titles in the admin interface
     def __str__(self):
         return f"{self.title} at {self.start_time} on {self.date}"
+    
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    floor_number = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} is Room {self.room_number} on Floor {self.floor_number}"
